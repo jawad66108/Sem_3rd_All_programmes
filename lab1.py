299a316aaa54364a6dc96d6581de5d9dca073d22
Jawad Hussain <jawad66108@gmail.com>
2025-10-14 18:05:44 +0500

    Add files via upload

---
+++ lab1.py
@@ -0,0 +1,9 @@
+# Write a Python program that stores and displays your personal
+# information, including your name, age, university, and hobby using the
+# print() function and variables
+name ="Jawad Hussain"
+age =20
+Uni ="UOW"
+Hobbies ="Football ,Riding ,Cricket"
+
+print("Name: ",name,"\nAge: ",age,"\nUniversity: ",Uni,"\nHobbies: ",Hobbies)
---
+++ lab2.py
@@ -0,0 +1,25 @@
+# Create a Python program that stores your current year and birth year in
+# variables. Calculate your age and print the result. Also, display the data
+# type of each variable using the type() function.
+
+# curr_year=2025;
+# bri_year=2005;
+# age=curr_year-bri_year
+
+# print("Age: ",age, type(age))
+# =========================================================================================
+
+# Write a Python program that takes two numbers and performs different
+# arithmetic operations on them, such as addition, subtraction,
+# multiplication, division, and modulus, and displays the results.
+
+n1 =60
+n2 =20
+print("Addition: ",n1+n2)
+print("Subtraction: ",n1-n2)
+print("Multiplication: ",n1*n2)
+print("Division: ",n1/n2)
+print("Mod: ",n1%n2)
+print("Floor Division: ",n1//n2)
+print("Floor Division: ",n1//n2)
+print("Exponentiation: ",n1**n2)
