20220a98a2ffc01ce95a2685c5f76817eba068fb
Jawad Hussain <jawad66108@gmail.com>
2025-10-19 11:34:43 +0500

    Add files via upload

---
+++ Lab1.py
@@ -0,0 +1,53 @@
+# name=input("Enter your name: ")
+# key =3
+
+# eny=""
+# for char in name:
+#     eny+=chr(ord(char)+key)
+# print("Encrypted message: "+eny)
+
+# dis=""
+# for char in eny:
+#     dis+=chr(ord(char)-key)
+# print("Discrypted message: "+dis)
+
+
+# import hashlib
+
+# pass=input(:="Reg Number: ")
+# encoded = pass.encoded()
+
+
+# name =input("Enter name: ")
+# key =2
+
+# eny=""
+
+# for i in name:
+#     eny+=chr(ord(i)+key)
+# print("Encrypted : ",eny)
+
+# dis=""
+
+# for i in eny:
+#     dis+=chr(ord(i)-key)
+# print("dicrypted: ",dis)
+
+# str = input("Enter your name: ")
+
+# key=3
+# eny,dis="",""
+# for i in str:
+#     eny+=chr(ord(i)+key)
+# print("Encrypted: ",eny)
+
+# for i in eny:
+#     dis+=chr(ord(i)-key)
+# print("decrypted: ",dis)
+
+# import hashlib
+# pas = input("Enter pasword: ")
+# enco =pas.encode()
+# npass =hashlib.sha256(enco)
+# dis = npass.hexdigest()
+# print("Password: ",dis)
