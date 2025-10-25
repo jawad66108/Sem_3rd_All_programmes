2902203e0ea89f1a73027116f37630b5e824a511
Jawad Hussain <jawad66108@gmail.com>
2025-10-25 15:11:24 +0500

    Add files via upload

---
+++ prep0`.cpp
@@ -0,0 +1,43 @@
+//#include<iostream>
+//#include<ctime>
+//#include<cstdlib>
+//using namespace std;
+//int main(){
+//	srand(0);
+//	for (int i=0;i<11;i++){
+//		cout<<rand()%150<<" ";
+//	}
+//}
+
+//==============================================
+//#include<iostream>
+//using namespace std;
+//struct Student{
+//	string name;
+//	int age;
+//	float marks;
+//};
+//int main(){
+//
+//Student s[3];
+//
+//for (int i=0;i<3;i++){
+//	cout<<"Name ,age ,marks\n";
+//	cin>>s[i].name>>s[i].age>>s[i].marks;
+//}
+//
+//for(int i=0;i<3;i++){
+//	cout<<s[i].name<<s[i].age<<s[i].marks<<"\n";
+//}
+//}
+
+//==================================================
+//#include<iostream>
+//#include<cmath>
+//using namespace std;
+//
+//int main(){
+//	int a=-24;
+//	cout<<abs(a);
+//}
+
