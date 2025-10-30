adb0bf40e2b1b71bb3b8b61c227c53235fbfc439
Jawad Hussain <jawad66108@gmail.com>
2025-10-30 18:51:20 +0500

    Add files via upload

---
+++ p2.cpp
@@ -0,0 +1,41 @@
+//Question 2 – 2D Array 
+//Write a C++ program that inputs a 3×3 matrix and: 
+//1. Displays the matrix in table form. 
+//2. Calculates and prints the sum of each row and each column. 
+//3. Displays the main diagonal elements. 
+
+
+//#include<iostream>
+//using namespace std;
+//int main(){
+//	int arr[3][3]={{24,25,26},{35,36,37},{63,66,68}};
+//	cout<<"============The original array=================\n";
+//	for(int i=0;i<3;i++){
+//		for(int m=0;m<3;m++){
+//			cout<<arr[i][m]<<" ";
+//		}
+//		cout<<endl;
+//	}
+//
+//	cout<<"============The Sum of Rows=================\n";
+//	for(int i=0;i<3;i++){
+//		int Rsum=0;
+//		for(int m=0;m<3;m++){
+//			Rsum+=arr[i][m];
+//			cout<<arr[i][m]<<" ";
+//		}
+//		cout<<" ----> "<<Rsum<<endl;
+//	}
+//	
+//		cout<<"============The Sum of Columns=================\n";
+//	for(int i=0;i<3;i++){
+//		int Csum=0;
+//		for(int m=0;m<3;m++){
+//			Csum+=arr[m][i];
+//			cout<<arr[i][m]<<" ";
+//		}
+//		cout<<" ----> "<<Csum<<endl;
+//	}
+//}
+
+
