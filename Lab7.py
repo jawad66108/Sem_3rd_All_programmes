fd8c5d90f82628b361ee40054bed5074c5248f03
Jawad Hussain <jawad66108@gmail.com>
2025-11-29 16:18:09 +0500

    Add files via upload

--- Lab7.py
+++ Lab7.py
@@ -1,114 +1,104 @@
-# from collections import deque
-
-# graph = {
-#     0: [1, 3, 4],
-#     1: [2],
-#     3: [5],
-#     4: [],
-#     2: [],
-#     5: []
-# }
-
-# def bfs(start):
-#     visited = set()
-#     queue = deque([start])
+def encryptRailFence(text, key):
+    rail = [['\n' for i in range(len(text))]
+                for j in range(key)]
+    # to find the direction
+    dir_down = False
+    row, col = 0, 0
+    for i in range(len(text)):
+        # check the direction of flow
+        # reverse the direction if we've just
+        # filled the top or bottom rail
+        if (row == 0) or (row == key - 1):
+            dir_down = not dir_down
+        
+        # fill the corresponding alphabet
+        rail[row][col] = text[i]
+        col += 1
+        
+        # find the next row using
+        # direction flag
+        if dir_down:
+            row += 1
+        else:
+            row -= 1
+    # now we can construct the cipher
+    # using the rail matrix
+    result = []
+    for i in range(key):
+        for j in range(len(text)):
+            if rail[i][j] != '\n':
+                result.append(rail[i][j])
+    return("" . join(result))
     
-#     while queue:
-#         node = queue.popleft()
-#         if node not in visited:
-#             print(node, end=" ")
-#             visited.add(node)
-#             queue.extend(graph[node])
 
-# print("BFS Traversal:")
-# bfs(0)
+def decryptRailFence(cipher, key):
 
-# DFS Graph (tree) from second image
-# tree = {
-#     1: [2, 7, 8],
-#     2: [3, 6],
-#     7: [],
-#     8: [9, 12],
-#     3: [4, 5],
-#     6: [],
-#     9: [10, 11],
-#     12: [],
-#     4: [],
-#     5: [],
-#     10: [],
-#     11: []
-# }
+    
+    rail = [['\n' for i in range(len(cipher))]
+                for j in range(key)]
+    
+    # to find the direction
+    dir_down = None
+    row, col = 0, 0
+    
+    # mark the places with '*'
+    for i in range(len(cipher)):
+        if row == 0:
+            dir_down = True
+        if row == key - 1:
+            dir_down = False
+        
+        # place the marker
+        rail[row][col] = '*'
+        col += 1
+        
+        # find the next row
+        # using direction flag
+        if dir_down:
+            row += 1
+        else:
+            row -= 1
+            
+    # now we can construct the
+    # fill the rail matrix
+    index = 0
+    for i in range(key):
+        for j in range(len(cipher)):
+            if ((rail[i][j] == '*') and (index < len(cipher))):
+                rail[i][j] = cipher[index]
+                index += 1
+        
+    # now read the matrix in
+    # zig-zag manner to construct
+    # the resultant text
+    result = []
+    row, col = 0, 0
+    for i in range(len(cipher)):
+        
+        # check the direction of flow
+        if row == 0:
+            dir_down = True
+        if row == key-1:
+            dir_down = False
+            
+        # place the marker
+        if (rail[row][col] != '*'):
+            result.append(rail[row][col])
+            col += 1
+            
+        # find the next row using
+        # direction flag
+        if dir_down:
+            row += 1
+        else:
+            row -= 1
+    return("".join(result))
 
-# def dfs(node, visited=set()):
-#     if node not in visited:
-#         print(node, end=" ")
-#         visited.add(node)
-#         for child in tree[node]:
-#             dfs(child, visited)
+# Driver code
+if __name__ == "__main__":
+    print(encryptRailFence("JAWAD ML", 5))
 
-# print("DFS Traversal:")
-# dfs(1)
+    # Now decryption of the
+    # same cipher-text
+    print(decryptRailFence("JALWMA D", 5))
 
-# from collections import deque
-# graph = {
-#     0: [1, 3, 4],
-#     1: [2],
-#     3: [5],
-#     4: [],
-#     2: [],
-#     5: []
-# }
-# def bfs(start):
-#     visited=set()
-#     queue= deque(start)
-
-#     while queue:
-#         node =queue.popleft()
-#         if node not in visited:
-#             visited.add(node)
-#             queue.extend(graph[node])
-
-
-# from collections import deque
-# graph = {
-#     0: [1, 3, 4],
-#     1: [2],
-#     3: [5],
-#     4: [],
-#     2: [],
-#     5: []
-# }
-
-# def bfs(start):
-#     visited =set()
-#     queue = deque([start])
-#     while queue:
-#         node = queue.popleft()
-#         if node not in visited:
-#             print(node)
-#             visited.add(node)
-#             # queue.extend(graph[node])
-#             for j in graph[node]:
-#                 if j not in visited:
-#                     queue.append(j)
-
-# bfs(0)
-
-# from collections import deque
-# graph = {
-#     0: [1, 3, 4],
-#     1: [2],
-#     3: [5],
-#     4: [],
-#     2: [],
-#     5: []
-# }
-
-# def dfs(node , visited=set()):
-#     if node not in visited:
-#         print(node)
-#         visited.add(node)
-#         for child in graph[node]:
-#             dfs(child,visited)
-
-# dfs(0)
