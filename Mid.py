7e4c81d62ea23aec4d6af6f341fcb459afb9fb05
Jawad Hussain <jawad66108@gmail.com>
2025-12-02 21:21:27 +0500

    Add files via upload

---
+++ Mid.py
@@ -0,0 +1,42 @@
+# import pandas as pd
+# df = pd.read_csv(r"C:\Users\Admin\OneDrive\Documents\Desktop\patients1.csv")
+# print(df)
+
+# max_score = 0
+
+# for i in df.iterrows():
+#     if df["health_index"] > max_score:
+#         max_score = df["health_index"]
+        
+
+
+# print(max_score)
+# for i in df:
+#     if df[i]["treatment_cost"] < 50000:
+#         print("Low treatmentcost ID:", i)
+
+
+# max2 =0 
+# for id,score in df:
+#     if maxS == score['health_score']:
+#         max2=score [id]['health_score']
+
+# print("Higest scores are: ",maxS,)
+
+# heat_readings = [72, 78, 75, 82 , 88 ,85 ,92]
+# minheat = heat_readings[0]
+# maxRise = 0
+
+# for i in heat_readings:
+#     if i < minheat:
+#         minheat = i
+
+# for i in heat_readings:
+#     rise = i-minheat
+#     if rise> maxRise:
+#         maxRise = rise
+
+# print("Max Temperature Rise:", maxRise)
+
+
+# print("Name: Jawad Hussain || DOB: 12-09-2005", " FatherName: Nasar Ullah || DOB: 1-05-1980", " GrandFather Name: Sher Hamza || DOB: 8-5-1900")
