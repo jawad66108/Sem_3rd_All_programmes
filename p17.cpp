9a0e2553b33e7aafb4c4af1d4b9826a558b7e4ed
Jawad Hussain <jawad66108@gmail.com>
2026-01-07 22:47:27 +0500

    Add files via upload

--- p17.cpp
+++ p17.cpp
@@ -1,76 +1,76 @@
-//Write C++ code for the following:
-//1. Declare the variables int quantity, float price, and totalBill for the inventory section.
-//2. Take input from the user for quantity and price, compute the total bill using totalBill =
-//quantity * price, and display the result.
-//3. Using two sample integer values, perform and display addition, subtraction,
-//multiplication, division, and modulus.
 #include<iostream>
 using namespace std;
+
+
+struct Node{
+	public:
+		Node * next;
+		int data;
+	
+};
+
+class ll{
+	Node * front ;
+	Node * rear ;
+	public:
+		ll(){
+			front =rear = NULL;
+		}
+		
+	void enqueue(int dat){
+			Node * newnode = new Node;
+			newode ->data = dat;
+			newnode -> next = NULL;
+		if (front == NULL){
+		
+			front = rear = NULL;
+		}
+		else {
+			rear ->next = newnode;
+			newnode -> next = NULL;
+			rear = newnode; 
+		}
+	}
+	
+	void dequeue (){
+		 if (front == NULL) {
+            cout << "Underflow\n";
+            return;
+        }
+        
+		Node  * temp = front ;
+		front = front ->next;
+		delete temp;
+	}
+};
+
 int main(){
-//	int quantity;
-//	float price;
-//	double totalBill;
-//	double a=0,b=0;
+//	const int N=8;
+//	int queue[N]= {20 , 30 ,60 ,80 ,50};
+//	int rear = -1, front =0;
+	
+//	int a;
+//	cout<<"Enter elenment you want to add: ";
+//	cin>>a;
 //	
-//	cout<<"Enter Quantity: ";
-//	cin>>quantity;
-//	cout<<"Enter Price: ";
-//	cin>>price;
-//	totalBill=quantity*price;
-//	cout<<"Your Total Bill is: "<<totalBill;
-//	
-//	cout<<"\nEnter 2 integers: ";
-//	cin>>a,b;
-//	
-//	cout<<"\nAddition: "<<a+b;
-//	cout<<"\nSubtraction: "<<a-b;
-//	cout<<"\nMultiplication: "<<a*b;
-//	if(a<b){
-//		cout<<"2 integer should be greater: ";
-//		cout<<"B: ";
-//		cin>>b;
-//	}else{
-//	cout<<"\nDivision: "<<a/b;
-//	}	
-//	cout<<"\nModulus%: "<<a%b;
-
-
-//Input marks of a student and display the grade using if-else or switch-case.
-//2. Use a for loop to print numbers from 1 to 10.
-//3. Use a while loop to calculate and display the sum of numbers from 1 to N, where N is
-//entered by the user.
-//Use a do-while loop to display the multiplication table of a number entered by the user.
-
-	int marks[3];
-	cout<<"Enter marks of three subjects: ";
-	
-	for(int i=0;i<3;i++){
-		cin>>marks[i];
-	}
-	int sum=0;
-	for(int i=0;i<3;i++){
-		cout<<i+1<<marks[i];
-		sum+=marks[i];
-	}
-	double avg = sum/3;
-	
-	cout<<"Avg: "<<avg;
-	
-	if(avg>85){
-		cout<<"You got A grade";
-	}else if(avg>75 && avg<85){
-		cout<<"You got B grade";
-	}else{
-		cout<<"Failed!!1\nbetter luck next time";
-	}
-	
-	int n;
-	cout<<"Enter n: ";
-	cin>>n;
-	int i=0,sum1=0;
-	while(i<n){
-		sum1+=i;
-		i++;
-	}
-	cout<<"Sum is: "<<sum;
+//	if (rear == N-1){
+//		cout<<"Overflow";
+//		return 0 ;
+//	}
+//	if (front >rear){
+//		cout<<"Underflow";
+//		return 0;
+//	}
+//	else{
+//		front ++;
+//		
+//	}
+//	else{
+//		rear = rear +1;
+//		queue [rear] = a;
+//		
+//	}
+//	for(int i=front ; i<=rear ; i++){
+//		cout<<queue[i]<<" ";
+//	}
 }
