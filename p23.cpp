6da821e3df1a9008ac221c82b994bbd47abca894
Jawad Hussain <jawad66108@gmail.com>
2026-01-24 10:49:21 +0500

    Add files via upload

--- p23.cpp
+++ p23.cpp
@@ -1,124 +1,104 @@
-//#include<iostream>
-//using namespace std;
-//
-//	const int N =5 ;
-//	int q[N];
-//	int front =-1 ,rear=-1;
-//	void enqueue(int data){
-//		if(rear==N-1){
-//			cout<<"Overflow";
-//			return ;
-//		}if(front==-1){
-//			front = 0 ;
-//		}
-//		rear = rear+1;
-//		q [rear] =data;
-//	}
-//	
-//	void dequeue(){
-//		if (front == -1 || front > rear){
-//			cout<<"Underflow";
-//			return ;
-//		}else{
-//			int d = q[front];
-//			front = front +1;
-//		}
-//	}
-//void display() {
-//    if (front == -1) {
-//        cout << "Queue is empty\n";
-//        return;
-//    }
-//
-//    for (int i = front; i <= rear; i++) {
-//        cout << q[i] << " ";
-//    }
-//    cout << endl;
-//}
-//
-//int main() {
-//    enqueue(10);
-//    enqueue(20);
-//    enqueue(30);
-//
-//    display();
-//
-//    dequeue();
-//    display();
-//
-//    return 0;
-//}
-
-
 #include<iostream>
 using namespace std;
 struct Node{
 	int data;
-	Node *next ;
+	Node * left;
+	Node * right;
 };
 
-class qe{
-	Node * front ;
-	Node *rear;
-	public:
-		qe(){
-			front = rear = NULL;
-		}
+Node* create(int val){
+	Node *newnode = new Node();
+	newnode->data=val;
+	newnode->left=NULL;
+	newnode->right=NULL;
+	return newnode;
+}
 
+Node* insert(Node *root,int val){
+	if(root == NULL){
+		return create(val);
+	}
+	if(val<root->data){
+		root->left=insert(root->left,val);
+	}
+	else if(val>root->data){
+		root->right=insert(root->right,val);
+	}
+	return root;
+}
 
-void enqueue(int dat){
-	Node * newnode = new Node;
-	if(front == NULL){
+void min(Node *root){
+	while(root->left!=NULL){
+		root=root->left;
+	}
+	cout<<"\nMin: "<<root->data;
+}
+
+void max(Node *root){
+	while(root->right!=NULL){
+		root=root->right;
+	}
+	cout<<"\nmax: "<<root->data;
+}
+
+void preorder(Node * root){
+	if(root!=NULL){
+	cout<<root->data<<" ";
+	preorder(root->left);
+	preorder(root->right);
+}
+}
+
+void inorder(Node * root){
+	if(root!=NULL){
+	inorder(root->left);
+	cout<<root->data<<" ";
+	inorder(root->right);
+}
+}
+
+void postorder(Node * root){
+	if(root!=NULL){
 	
-		newnode -> data = dat ;
-		newnode -> next =NULL;
-		front = rear = newnode;
+	postorder(root->left);
+	postorder(root->right);
+	cout<<root->data<<" ";
+}
+}
+
+int nodes(Node *root){
+	if(root==NULL){
+		return 0;
+	}
+	
+	return 1+ nodes(root->left) + nodes(root->right);
+}
+
+
+
+int main(){
+	Node * root = NULL;
+	int val;
+	
+	cout<<"Enter 4 numbers : ";
+	for(int i=0;i<4;i++){
+		cin>>val;
+		root= insert(root,val);
+	}
+	
+	cout<<"\n";
+	min(root);
+	max(root);
+	
+	cout<<endl;
+	cout<<"Pre-order: ";
+	preorder(root);
+	cout<<"\nIn-order: ";
+	inorder(root);
+	cout<<"\nPost-order: ";
+	postorder(root);
+	
+	cout<<"\nTotal nodes: "<<nodes(root);
 		
-	}
-	else {
-		newnode -> data = dat ;
-		newnode -> next = NULL;
-		rear ->next =newnode;
-		rear = newnode;
-		
-	}
 }
 
-void dequeue(){
-	Node * temp = front ;
-	if (front == NULL){
-		cout<<"Underflow";
-		return;
-	}
-	front = front -> next;
-	delete temp;
-}
- void display() {
-        if (front == NULL) {
-            cout << "Queue is empty\n";
-            return;
-        }
-
-        Node* temp = front;
-        while (temp != NULL) {
-            cout << temp->data << " ";
-            temp = temp->next;
-        }
-        cout << endl;
-    }
-};
-
-int main() {
-    qe q;
-
-    q.enqueue(10);
-    q.enqueue(20);
-    q.enqueue(30);
-
-    q.display();
-
-    q.dequeue();
-    q.display();
-
-    return 0;
-}
